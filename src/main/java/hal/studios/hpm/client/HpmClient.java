f54657489b02c49982b7211b632f23fcb288c111
JoshM487 <89715250+JoshM487@users.noreply.github.com>
2026-08-16 17:49:48 -0400

    Validate datapack resources, ship persistence, and rendering

--- src/main/java/hal/studios/hpm/client/HpmClient.java
+++ src/main/java/hal/studios/hpm/client/HpmClient.java
@@ -15,18 +15,18 @@ public final class HpmClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HpmShipModelLayers.register();
-        register(HpmEntities.RAFT, HpmShipModelLayers.RAFT, "raft.png");
-        register(HpmEntities.SWASHBUCKLER, HpmShipModelLayers.SWASHBUCKLER, "swashbuckler.png");
-        register(HpmEntities.SWASHBUCKLER_UPGRADED, HpmShipModelLayers.SWASHBUCKLER_UPGRADED, "swashbucklerupgraded.png");
-        register(HpmEntities.CUTTER, HpmShipModelLayers.CUTTER, "cutterremastered.png");
-        register(HpmEntities.CUTTER_MILITARISED, HpmShipModelLayers.CUTTER_WEAPONISED, "cutterweaponisedremastered.png");
-        register(HpmEntities.CUTTER_PIRATE, HpmShipModelLayers.CUTTER_WEAPONISED, "cutterpirateremastered.png");
-        register(HpmEntities.CORVETTE_STEAMSHIP, HpmShipModelLayers.CORVETTE, "corvetteclass.png");
+        register(HpmEntities.RAFT, HpmShipModelLayers.RAFT, "raft.png", "hpm:raft");
+        register(HpmEntities.SWASHBUCKLER, HpmShipModelLayers.SWASHBUCKLER, "swashbuckler.png", "hpm:swashbuckler");
+        register(HpmEntities.SWASHBUCKLER_UPGRADED, HpmShipModelLayers.SWASHBUCKLER_UPGRADED, "swashbucklerupgraded.png", "hpm:swashbucklerupgraded");
+        register(HpmEntities.CUTTER, HpmShipModelLayers.CUTTER, "cutterremastered.png", "hpm:cutter");
+        register(HpmEntities.CUTTER_MILITARISED, HpmShipModelLayers.CUTTER_WEAPONISED, "cutterweaponisedremastered.png", "hpm:cuttermilitarised");
+        register(HpmEntities.CUTTER_PIRATE, HpmShipModelLayers.CUTTER_WEAPONISED, "cutterpirateremastered.png", "hpm:cutter_pirate");
+        register(HpmEntities.CORVETTE_STEAMSHIP, HpmShipModelLayers.CORVETTE, "corvetteclass.png", "hpm:corvette_steamship");
     }
 
     @SuppressWarnings({"rawtypes", "unchecked"})
-    private static void register(EntityType<? extends AbstractBoat> type, ModelLayerLocation layer, String textureName) {
+    private static void register(EntityType<? extends AbstractBoat> type, ModelLayerLocation layer, String textureName, String debugId) {
         Identifier texture = HpmMod.id("textures/entities/" + textureName);
-        EntityRenderers.register((EntityType) type, context -> new HpmShipRenderer(context, layer, texture));
+        EntityRenderers.register((EntityType) type, context -> new HpmShipRenderer(context, layer, texture, debugId));
     }
 }
--- src/main/java/hal/studios/hpm/client/renderer/HpmShipRenderer.java
+++ src/main/java/hal/studios/hpm/client/renderer/HpmShipRenderer.java
@@ -1,8 +1,11 @@
 package hal.studios.hpm.client.renderer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
 import hal.studios.hpm.client.model.HpmShipModel;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.AbstractBoatRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.state.BoatRenderState;
@@ -10,14 +13,27 @@ import net.minecraft.resources.Identifier;
 
 public final class HpmShipRenderer extends AbstractBoatRenderer {
     private final EntityModel<BoatRenderState> shipModel;
+    private final String debugId;
+    private boolean renderLogged;
 
-    public HpmShipRenderer(EntityRendererProvider.Context context, ModelLayerLocation layer, Identifier texture) {
+    public HpmShipRenderer(EntityRendererProvider.Context context, ModelLayerLocation layer, Identifier texture, String debugId) {
         super(context, texture);
         this.shipModel = new HpmShipModel(context.bakeLayer(layer));
+        this.debugId = debugId;
     }
 
     @Override
     protected EntityModel<BoatRenderState> model() {
         return this.shipModel;
+    }
+
+    @Override
+    public void render(BoatRenderState state, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
+        super.render(state, poseStack, bufferSource, packedLight);
+
+        if (Boolean.getBoolean("hpm.ci.renderTest") && !this.renderLogged) {
+            this.renderLogged = true;
+            System.out.println("SWASHBUCKLERS_RENDER_OK " + this.debugId);
+        }
     }
 }
